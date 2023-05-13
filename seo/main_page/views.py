236ea4a14bbf5f73204747cb1ac4ac8bf01b80d8
tbtgmltn97 <tbtgmltn97@naver.com>
2023-05-13 18:08:45 +0900

    good

--- seo/main_page/views.py
+++ seo/main_page/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'main_page/index.html')
+
+def rolling(request):
+    return render(request, 'main_page/rolling_paper.html')
+
+def profile(request):
+    return render(request, 'main_page/profile.html')
+
+def quotes(request):
+    return render(request, 'main_page/quotes.html')
--- seo/seo/urls.py
+++ seo/seo/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main_page.urls')), # 127.0.0.1로 접속하면 알아서 main_page url로 이동하게끔!
 ]

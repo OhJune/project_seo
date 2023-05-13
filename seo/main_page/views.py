0846c43897bbe12644ec604bf785140197af2dcd
tbtgmltn97 <tbtgmltn97@naver.com>
2023-05-13 23:06:44 +0900

    tired

--- seo/main_page/views.py
+++ seo/main_page/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from common.views import signup
 
-# Create your views here.
-
+@login_required
 def index(request):
     return render(request, 'main_page/index.html')
 
 def rolling(request):
     return render(request, 'main_page/rolling_paper.html')
 
+@login_required
 def profile(request):
     return render(request, 'main_page/profile.html')
 
+@login_required
 def quotes(request):
     return render(request, 'main_page/quotes.html')
--- seo/seo/settings.py
+++ seo/seo/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_page.apps.MainPageConfig',
+    'common.apps.CommonConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'seo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER' : 'root',
+        'PASSWORD' : '12345678',
+        'HOST' : 'jibeen-mysql.c9mnzpvc8vyh.ap-northeast-2.rds.amazonaws.com',
+        'PORT' : '3306'
     }
 }
 
